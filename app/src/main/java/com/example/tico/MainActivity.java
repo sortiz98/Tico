266e2e955426a5b5eb102a6b984585cd9bbc6d6e
Qitian Liao <liaoqitian1024@gmail.com>
2020-10-30 18:12:43 -0400

    google location works

--- app/src/main/java/com/example/tico/MainActivity.java
+++ app/src/main/java/com/example/tico/MainActivity.java
@@ -7,6 +7,8 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -24,6 +26,11 @@ import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    TextView test;
+    Button currentLocation;
+
+
     // Type conversion:     static String API_KEY = R.string.Google_API_Key;
     static String GEO_URL = "https://maps.googleapis.com/maps/api/geocode/json";
     String address = ""; // might not need
@@ -37,6 +44,9 @@ public class MainActivity extends AppCompatActivity {
 
     /** TODO */
     String type = "currentLocation";
+    String name;
+    JSONArray results;
+
 
 
     @Override
@@ -44,32 +54,35 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (type.equals("currentLocation")) {
-            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
-                    ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
-            }
-            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
-                @Override
-                public void onSuccess(Location location) {
-                    double lat = location.getLatitude(), lon = location.getLongitude();
-                    findRestaurantsByCoordinate(lat, lon);
-                }
-            });
+        test = findViewById(R.id.test);
+        currentLocation = findViewById(R.id.currentLocation);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 44);
         }
+        fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                double lat = location.getLatitude(), lon = location.getLongitude();
+                findRestaurantsByCoordinate(lat, lon);
+            }
+        });
 
     }
 
     private void findRestaurantsByCoordinate(double lat, double lon) {
         RequestQueue queue = Volley.newRequestQueue(this);
         String gps_URL = "location=-" + lat + "," + lon + "&radius=1500&type=restaurant";
-        String full_URL = restaurant_URL + gps_URL + "&key=" + R.string.Google_API_Key;
+        String full_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyDugNQO9vZxbi68BQnReZCd_CeM-cg-WW0";
+//        String full_URL = restaurant_URL + gps_URL + "&key=" + getResources().getString(R.string.Google_API_Key);
         JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, full_URL, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
-                    address = ((JSONArray) response.get("results")).getJSONObject(0).getString("formatted_address");
+                    results = (JSONArray) response.get("results");
+                    name = results.getJSONObject(0).getString("name");
+                    test.setText(name);
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -78,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onErrorResponse(VolleyError error) {
                 String jsonError = new String(error.networkResponse.data);
-                System.out.println(jsonError);
+                test.setText(jsonError);
             }
         });
         queue.add(stringRequest);
