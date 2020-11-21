ae2fc6d92d187c673b917f587b5c1ad92ea7a996
Qitian Liao <liaoqitian1024@gmail.com>
2020-11-21 00:40:24 -0500

    delete space

--- app/src/main/java/com/example/tico/MainActivity.java
+++ app/src/main/java/com/example/tico/MainActivity.java
@@ -63,7 +63,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         language = getIntent().getExtras().getString("language");
